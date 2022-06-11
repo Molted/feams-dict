608d54cc2e4ff3599258a0f6ab49885056cf4219
eudesaugustinesilerio <72860112+eudesaugustinesilerio@users.noreply.github.com>
2022-06-11 23:21:05 +0700

    Added dropdown list

--- app/Views/adminlte.php
+++ app/Views/adminlte.php
@@ -7,7 +7,8 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="<?= base_url()?>/public/dist/adminlte/plugins/fontawesome-free/css/all.min.css">
+  <!-- <link rel="stylesheet" href="<?= base_url()?>/public/dist/adminlte/plugins/fontawesome-free/css/all.min.css"> -->
+  
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Tempusdominus Bbootstrap 4 -->
@@ -25,6 +26,7 @@
   <!-- DataTables -->
   <link rel="stylesheet" href="<?= base_url()?>/public/dist/adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
   <link rel="stylesheet" href="<?= base_url()?>/public/dist/adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <?= $this->renderSection('styles');?>
 </head>
 <body class="hold-transition sidebar-mini">
