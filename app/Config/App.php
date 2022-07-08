fc4150cf4e0f1bc64fcd37839c85cb094e04ebdd
Molted <joshuaconcepcion12.jc@gmail.com>
2022-07-08 19:33:06 +0800

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
 	 * @var string
 	 */
   
-	public $baseURL = 'http://localhost:8080/feams-test/';
+	public $baseURL = 'http://localhost/feams-test/';
 
 	/**
 	 * --------------------------------------------------------------------------
