c97d910cbb4b5859a62aeaa960f5a5b578dfc102
Molted <joshuaconcepcion12.jc@gmail.com>
2022-07-13 21:40:21 +0800

    Fixed bug (Breadcrumb Home)

--- modules/FileSharing/Views/index.php
+++ modules/FileSharing/Views/index.php
@@ -7,8 +7,8 @@
         </div>
         <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="#">Home</a></li>
-            <li class="breadcrumb-item active"><?= esc($title)?></li>
+            <li class="breadcrumb-item"><a href="<?=base_url()?>">Home</a></li>
+            <li class="breadcrumb-item active"><?=esc($title)?></li>
         </ol>
     </div>
 </div>
