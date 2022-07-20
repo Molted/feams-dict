b6179ed925160a6e0d74b6772c762f7c0f299c03
Molted <joshuaconcepcion12.jc@gmail.com>
2022-07-20 15:04:10 +0800

    Updated Pop up message (Latest News)

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -3,11 +3,13 @@
 use DateTime;
 use DateInterval;
 use App\Models as Models;
+use Modules\NewsEvents\Models as NewsModels;
 
 class Login extends BaseController {
     public function __construct() {
         $this->userModel = new Models\UserModel();
         $this->loginModel = new Models\LoginModel();
+        $this->newsModel = new NewsModels\NewsModel();
         $this->resetPasswordModel = new Models\ResetPasswordModel();
     }
 
@@ -49,11 +51,31 @@ class Login extends BaseController {
                     return redirect()->back()->withInput(); 
                 }
                 $this->setUserSession($user);
+                $data['firstNews'] = $this->newsModel->orderBy('created_at', 'DESC')->first();
+                // echo "<pre>";
+                // die(print_r($data));
                 if($user['role'] == '1'){
-                    $this->session->setFlashData('welcomeMsg', 'Welcome to FEAMS!');
+                    if(!empty($data['firstNews'])){
+                        $this->session->setFlashData('Featured', $data['firstNews']['image']);
+                        $this->session->setFlashData('Content', 
+                            $data['firstNews']['title'] .'<br><br>'.
+                            $data['firstNews']['content'].'<br><a href="'.
+                            base_url().'">See more...</a>'
+                        );
+                    }
+                    
+                    $this->session->setFlashData('NoNews', 'No Latest News!');
                     return redirect()->withInput()->to(base_url('admin/dashboard'));
                 } else {
-                    $this->session->setFlashData('welcomeMsg', 'Welcome to FEAMS!');
+                    if(!empty($data['firstNews'])){
+                        $this->session->setFlashData('Featured', $data['firstNews']['image']);
+                        $this->session->setFlashData('Content', 
+                            $data['firstNews']['title'] .'<br><br>'.
+                            $data['firstNews']['content'].'<br><a href="'.
+                            base_url().'">See more...</a>'
+                        );
+                    }
+                    $this->session->setFlashData('NoNews', 'No Latest News!');
                     return redirect()->withInput()->to(base_url('user/'.$user['username']));
                 }
             }
--- modules/Users/Views/profile.php
+++ modules/Users/Views/profile.php
@@ -550,48 +550,56 @@ foreach($perm_id['perm_id'] as $perms) {
     nextSibling.innerText = name
     })
 </script>
-<?php if(!empty(session()->getFlashdata('welcomeMsg'))):?>
-	<!-- SweetAlert JS -->
-	<script src="<?= base_url();?>/public/js/sweetalert.min.js"></script>
-	<script src="<?= base_url();?>/public/js/sweetalert2.all.min.js"></script>
+<!-- SweetAlert JS -->
+<script src="<?= base_url();?>/public/js/sweetalert.min.js"></script>
+<script src="<?= base_url();?>/public/js/sweetalert2.all.min.js"></script>
+<?php if(!empty(session()->getFlashdata('Content'))):?>
 	<script>
 		window.onload = function() {
-
-			Swal.fire({
-				icon: 'success',
-				title: 'Success!',
-				text: '<?= session()->getFlashdata('welcomeMsg');?>',
-				confirmButtonColor: '#3085d6',
-				confirmButtonText: 'Okay'
-			})/*swal2*/.then((result) =>
+      Swal.fire(
+            'Success Login!',
+            'Welcome to FEAMS!',
+            'success'
+          )
+			/*swal2*/.then((result) =>
 			{
 				/* Read more about isConfirmed, isDenied below */
 				if (result.isConfirmed)
 				{
-					Swal.close()
+          Swal.fire({
+            title: '<b>Our Latest News!</b>',
+            imageUrl: '<?= base_url()?>/public/uploads/news/<?= session()->getFlashdata('Featured');?>',
+            html: '<?=session()->getFlashdata('Content');?>',
+            imageWidth: 400,
+            imageHeight: 250,
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Okay'
+          })
 				}
 			})//then
 		};
-	</script>
-<?php elseif(!empty(session()->getFlashdata('failMsg'))):?>
-	<script>
-		window.onload = function() {
-
-			Swal.fire({
-				icon: 'error',
-				title: 'Error!',
-				text: '<?= session()->getFlashdata('failMsg');?>',
-				confirmButtonColor: '#3085d6',
-				confirmButtonText: 'Okay'
-			})/*swal2*/.then((result) =>
-			{
-				/* Read more about isConfirmed, isDenied below */
-				if (result.isConfirmed)
-				{
-					Swal.close()
-				}
-			})//then
-		};
-	</script>
+  </script>
+<?php elseif(!empty(session()->getFlashdata('NoNews'))):?>
+  <script>
+    window.onload = function() {
+      Swal.fire(
+            'Success Login!',
+            'Welcome to FEAMS!',
+            'success'
+          )
+      /*swal2*/.then((result) =>
+      {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {          
+          Swal.fire({
+            icon: 'question',
+            html: '<?= session()->getFlashdata('NoNews');?>',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Okay'
+          })
+        }
+      })//then
+    };
+  </script>
 <?php endif;?>
 <?= $this->endSection() ?>
